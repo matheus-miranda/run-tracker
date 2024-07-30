9f36f82fe14f6f9c17dd56f72119051a8460c041
Matheus Miranda <15269393+matheus-miranda@users.noreply.github.com>
2024-07-29 23:22:25 -0300

    implement material theme

---
+++ core/presentation/designsystem/src/main/java/com/msmlabs/core/presentation/designsystem/Theme.kt
@@ -0,0 +1,43 @@
+package com.msmlabs.core.presentation.designsystem
+
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+val DarkColorScheme = darkColorScheme(
+    primary = RunTrackerGreen,
+    background = RunTrackerBlack,
+    surface = RunTrackerDarkGray,
+    secondary = RunTrackerWhite,
+    tertiary = RunTrackerWhite,
+    primaryContainer = RunTrackerGreen30,
+    onPrimary = RunTrackerBlack,
+    onBackground = RunTrackerWhite,
+    onSurface = RunTrackerWhite,
+    onSurfaceVariant = RunTrackerGray,
+)
+
+@Composable
+fun RunTrackerTheme(
+    content: @Composable () -> Unit
+) {
+    val colorScheme = DarkColorScheme
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
