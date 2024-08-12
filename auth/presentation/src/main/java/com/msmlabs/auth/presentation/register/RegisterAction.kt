1b8b98367b8a25397eacda63ae5498f2b2af31d3
Matheus Miranda <15269393+matheus-miranda@users.noreply.github.com>
2024-08-12 00:59:03 -0300

    registration events

--- auth/presentation/src/main/java/com/msmlabs/auth/presentation/register/RegisterAction.kt
+++ auth/presentation/src/main/java/com/msmlabs/auth/presentation/register/RegisterAction.kt
@@ -1,5 +1,8 @@
 package com.msmlabs.auth.presentation.register
 
+/**
+ * Actions are passed on from the UI to the Model
+ */
 sealed interface RegisterAction {
     data object OnTogglePasswordVisibilityClick : RegisterAction
     data object OnLoginClick : RegisterAction
