8308da3190da82f3bff9720f6ba94b0328c3102c
Matheus Miranda <15269393+matheus-miranda@users.noreply.github.com>
2024-08-26 01:41:26 -0300

    create runtracker scaffold

---
+++ core/presentation/designsystem/src/main/java/com/msmlabs/core/presentation/designsystem/components/RuntrackerScaffold.kt
@@ -0,0 +1,31 @@
+package com.msmlabs.core.presentation.designsystem.components
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RuntrackerScaffold(
+    content: @Composable (PaddingValues) -> Unit,
+    modifier: Modifier = Modifier,
+    withGradient: Boolean = true,
+    topAppBar: @Composable () -> Unit = {},
+    floatingActionButton: @Composable () -> Unit = {},
+) {
+    Scaffold(
+        topBar = topAppBar,
+        floatingActionButton = floatingActionButton,
+        floatingActionButtonPosition = FabPosition.Center,
+        modifier = modifier,
+    ) { paddingValues ->
+        if (withGradient) {
+            GradientBackground {
+                content(paddingValues)
+            }
+        } else {
+            content(paddingValues)
+        }
+    }
+}
