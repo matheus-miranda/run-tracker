b61b803ca9c00561d7c7d7a281c1bd30a8c367b7
Matheus Miranda <15269393+matheus-miranda@users.noreply.github.com>
2024-08-31 21:05:48 -0300

    request permissions on active run screen

--- run/presentation/src/main/java/com/msmlabs/run/presentation/active_run/ActiveRunScreen.kt
+++ run/presentation/src/main/java/com/msmlabs/run/presentation/active_run/ActiveRunScreen.kt
@@ -2,6 +2,13 @@
 
 package com.msmlabs.run.presentation.active_run
 
+import android.Manifest
+import android.content.Context
+import android.os.Build
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,18 +17,26 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.msmlabs.core.presentation.designsystem.RunTrackerTheme
 import com.msmlabs.core.presentation.designsystem.StartIcon
 import com.msmlabs.core.presentation.designsystem.StopIcon
+import com.msmlabs.core.presentation.designsystem.components.RuntrackerDialog
 import com.msmlabs.core.presentation.designsystem.components.RuntrackerFloatingActionButton
+import com.msmlabs.core.presentation.designsystem.components.RuntrackerOutlinedActionButton
 import com.msmlabs.core.presentation.designsystem.components.RuntrackerScaffold
 import com.msmlabs.core.presentation.designsystem.components.RuntrackerToolbar
 import com.msmlabs.run.presentation.R
 import com.msmlabs.run.presentation.active_run.components.RunDataCard
+import com.msmlabs.run.presentation.util.hasLocationPermission
+import com.msmlabs.run.presentation.util.hasNotificationPermission
+import com.msmlabs.run.presentation.util.shouldShowLocationPermissionRationale
+import com.msmlabs.run.presentation.util.shouldShowNotificationPermissionRationale
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -40,6 +55,58 @@ private fun ActiveRunScreen(
     onAction: (ActiveRunAction) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val context = LocalContext.current
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions()
+    ) { perms ->
+        val hasCoarseLocationPermission = perms[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+        val hasFineLocationPermission = perms[Manifest.permission.ACCESS_FINE_LOCATION] == true
+        val hasNotificationPermission = if (Build.VERSION.SDK_INT >= 33) {
+            perms[Manifest.permission.POST_NOTIFICATIONS] == true
+        } else true
+
+        val activity = context as ComponentActivity
+        val showLocationRationale = activity.shouldShowLocationPermissionRationale()
+        val showNotificationRationale = activity.shouldShowNotificationPermissionRationale()
+
+        onAction(
+            ActiveRunAction.SubmitLocationPermissionInfo(
+                acceptedLocationPermission = hasCoarseLocationPermission && hasFineLocationPermission,
+                showLocationRationale = showLocationRationale
+            )
+        )
+        onAction(
+            ActiveRunAction.SubmitNotificationPermissionInfo(
+                acceptedNotificationPermission = hasNotificationPermission,
+                showNotificationPermissionRationale = showNotificationRationale
+            )
+        )
+    }
+
+    // Requesting when screen initially loads
+    LaunchedEffect(key1 = true) {
+        val activity = context as ComponentActivity
+        val showLocationRationale = activity.shouldShowLocationPermissionRationale()
+        val showNotificationRationale = activity.shouldShowNotificationPermissionRationale()
+
+        onAction(
+            ActiveRunAction.SubmitLocationPermissionInfo(
+                acceptedLocationPermission = context.hasLocationPermission(),
+                showLocationRationale = showLocationRationale
+            )
+        )
+        onAction(
+            ActiveRunAction.SubmitNotificationPermissionInfo(
+                acceptedNotificationPermission = context.hasNotificationPermission(),
+                showNotificationPermissionRationale = showNotificationRationale
+            )
+        )
+
+        if (!showLocationRationale && !showNotificationRationale) {
+            permissionLauncher.requestRuntrackerPermissions(context)
+        }
+    }
+
     RuntrackerScaffold(
         withGradient = false,
         topAppBar = {
@@ -79,6 +146,54 @@ private fun ActiveRunScreen(
             }
         }
     )
+
+    if (state.showLocationRationale || state.showNotificationRationale) {
+        RuntrackerDialog(
+            title = stringResource(id = R.string.permission_required),
+            onDismiss = { /* Normal dismissing not allowed for permissions. */ },
+            description = when {
+                state.showLocationRationale && state.showNotificationRationale -> {
+                    stringResource(id = R.string.location_notification_rationale)
+                }
+
+                state.showLocationRationale -> {
+                    stringResource(id = R.string.location_rationale)
+                }
+
+                else -> stringResource(id = R.string.notification_rationale)
+            },
+            primaryButton = {
+                RuntrackerOutlinedActionButton(text = stringResource(id = R.string.ok), isLoading = false, onClick = {
+                    onAction(ActiveRunAction.DismissRationaleDialog)
+                    permissionLauncher.requestRuntrackerPermissions(context)
+                })
+            }
+        )
+    }
+}
+
+private fun ActivityResultLauncher<Array<String>>.requestRuntrackerPermissions(
+    context: Context,
+) {
+    val hasLocationPermission = context.hasLocationPermission()
+    val hasNotificationPermission = context.hasNotificationPermission()
+
+    val locationPermissions = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+    )
+    val notificationPermissions = if (Build.VERSION.SDK_INT >= 33) {
+        arrayOf(Manifest.permission.POST_NOTIFICATIONS)
+    } else arrayOf()
+
+    when {
+        !hasLocationPermission && !hasNotificationPermission -> {
+            launch(locationPermissions + notificationPermissions)
+        }
+
+        !hasLocationPermission -> launch(locationPermissions)
+        !hasNotificationPermission -> launch(notificationPermissions)
+    }
 }
 
 @Preview
