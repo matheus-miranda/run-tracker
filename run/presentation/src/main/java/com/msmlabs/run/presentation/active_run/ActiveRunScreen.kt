2dd2e6ea779ca434407bfbf104f62e0163151c37
Matheus Miranda <15269393+matheus-miranda@users.noreply.github.com>
2024-09-08 00:27:50 -0300

    track path on google map

--- run/presentation/src/main/java/com/msmlabs/run/presentation/active_run/ActiveRunScreen.kt
+++ run/presentation/src/main/java/com/msmlabs/run/presentation/active_run/ActiveRunScreen.kt
@@ -33,6 +33,7 @@ import com.msmlabs.core.presentation.designsystem.components.RuntrackerScaffold
 import com.msmlabs.core.presentation.designsystem.components.RuntrackerToolbar
 import com.msmlabs.run.presentation.R
 import com.msmlabs.run.presentation.active_run.components.RunDataCard
+import com.msmlabs.run.presentation.active_run.maps.TrackerMap
 import com.msmlabs.run.presentation.util.hasLocationPermission
 import com.msmlabs.run.presentation.util.hasNotificationPermission
 import com.msmlabs.run.presentation.util.shouldShowLocationPermissionRationale
@@ -136,6 +137,13 @@ private fun ActiveRunScreen(
                     .fillMaxSize()
                     .background(MaterialTheme.colorScheme.surface)
             ) {
+                TrackerMap(
+                    isRunFinished = state.isRunFinished,
+                    currentLocation = state.currentLocation,
+                    locations = state.runData.locations,
+                    onSnapshot = {},
+                    modifier = Modifier.fillMaxSize()
+                )
                 RunDataCard(
                     elapsedTime = state.elapsedTime, runData = state.runData,
                     modifier = Modifier
