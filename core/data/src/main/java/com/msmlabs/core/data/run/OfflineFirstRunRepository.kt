9047e8745d603f32fec80b5dfeff2b717fc24fe2
Matheus Miranda <15269393+matheus-miranda@users.noreply.github.com>
2024-10-13 20:28:38 -0300

    saving pending syncs

--- core/data/src/main/java/com/msmlabs/core/data/run/OfflineFirstRunRepository.kt
+++ core/data/src/main/java/com/msmlabs/core/data/run/OfflineFirstRunRepository.kt
@@ -1,5 +1,8 @@
 package com.msmlabs.core.data.run
 
+import com.msmlabs.core.database.dao.RunPendingSyncDao
+import com.msmlabs.core.database.mapper.toRun
+import com.msmlabs.core.domain.SessionStorage
 import com.msmlabs.core.domain.run.LocalRunDataSource
 import com.msmlabs.core.domain.run.RemoteRunDataSource
 import com.msmlabs.core.domain.run.Run
@@ -10,13 +13,18 @@ import com.msmlabs.core.domain.util.EmptyResult
 import com.msmlabs.core.domain.util.Result
 import com.msmlabs.core.domain.util.asEmptyDataResult
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class OfflineFirstRunRepository(
     private val localRunDataSource: LocalRunDataSource,
     private val remoteRunDataSource: RemoteRunDataSource,
     private val applicationScope: CoroutineScope,
+    private val runPendingSyncDao: RunPendingSyncDao,
+    private val sessionStorage: SessionStorage,
 ) : RunRepository {
 
     override fun getRuns(): Flow<List<Run>> {
@@ -61,8 +69,59 @@ class OfflineFirstRunRepository(
     override suspend fun deleteRun(id: RunId) {
         localRunDataSource.deleteRun(id)
 
+        // Edge case where run is created and deleted in offline-mode.
+        // Then we don't need to sync.
+        val isPendingSync = runPendingSyncDao.getRunPendingSyncEntity(id) != null
+        if (isPendingSync) {
+            runPendingSyncDao.deleteRunPendingSyncEntity(id)
+            return
+        }
+
         val remoteResult = applicationScope.async {
             remoteRunDataSource.deleteRun(id)
         }.await()
+    }
+
+    override suspend fun syncPendingRuns() {
+        withContext(Dispatchers.IO) {
+            val userId = sessionStorage.get()?.userId ?: return@withContext
+
+            val createdRuns = async {
+                runPendingSyncDao.getAllRunPendingSyncEntities(userId)
+            }
+            val deletedRuns = async {
+                runPendingSyncDao.getAllDeletedRunSyncEntities(userId)
+            }
+
+            val createJobs = createdRuns
+                .await()
+                .map {
+                    launch {
+                        val run = it.run.toRun()
+                        when (val result = remoteRunDataSource.postRun(run, it.mapPictureBytes)) {
+                            is Result.Error -> Unit
+                            is Result.Success -> applicationScope.launch {
+                                runPendingSyncDao.deleteRunPendingSyncEntity(it.runId)
+                            }.join()
+                        }
+                    }
+                }
+
+            val deleteJobs = deletedRuns
+                .await()
+                .map {
+                    launch {
+                        when (val result = remoteRunDataSource.deleteRun(it.runId)) {
+                            is Result.Error -> Unit
+                            is Result.Success -> applicationScope.launch {
+                                runPendingSyncDao.deleteDeletedRunSyncEntity(it.runId)
+                            }.join()
+                        }
+                    }
+                }
+
+            createJobs.forEach { it.join() }
+            deleteJobs.forEach { it.join() }
+        }
     }
 }
